067638e59e21c8c93695d95f3defd9f5f77c1dd0
qwe321qwe321qwe321 <aegis321123@gmail.com>
2020-07-10 11:42:14 +0800

    first commit

---
+++ Assets/ScriptTemplates/Editor/NamespaceFiller.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * NamespaceFiller is to replace the "#NAMESPACE#" string into the specific namespace made by yourself when you create a new C# script.
+ * It has to be used with the custum script template which has "#NAMESPACE#" keyword.
+ * Created by PeDev 2020
+ */
+
+using UnityEngine;
+using UnityEditor;
+
+namespace Pedev.EditorExtensions {
+	public class NamespaceFiller : UnityEditor.AssetModificationProcessor {
+		public static void OnWillCreateAsset(string path) {
+			path = path.Replace(".meta", "");
+			int index = path.LastIndexOf(".");
+			if (index < 0) return;
+			string file = path.Substring(index);
+			if (file != ".cs") return;
+			index = Application.dataPath.LastIndexOf("Assets");
+			path = Application.dataPath.Substring(0, index) + path;
+			file = System.IO.File.ReadAllText(path);
+
+			// Replace Namespace.
+			string _namespace = GetNamespaceByPath(path);
+			file = file.Replace("#NAMESPACE#", _namespace);
+
+			// Replace script name without editor.
+			index = path.LastIndexOf('/') + 1;
+			int extIndex = path.LastIndexOf('.');
+			string filename = path.Substring(index, extIndex - index);
+			file = file.Replace("#SCRIPTNAME_WITHOUT_EDITOR#", filename.Replace("Editor", ""));
+
+			System.IO.File.WriteAllText(path, file);
+			AssetDatabase.Refresh();
+		}
+
+		private static string GetNamespaceByPath(string path) {
+			string lastPart = path.Substring(path.IndexOf("Assets"));
+			lastPart = lastPart.Substring("Assets/".Length);
+			string _namespace = lastPart.Substring(0, lastPart.LastIndexOf('/'));
+			_namespace = _namespace.Replace("/Scripts", "");
+			_namespace = _namespace.Replace("/Editor", "");
+			_namespace = _namespace.Replace('/', '.');
+			return _namespace;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Unity-ScriptTemplates
-The script templates and namespace filler of Unity. It is just a backup for personal use.
+There are some script templates and the namespace filler script for Unity.
+
+It is just a backup for personal use and it's a constant work in progress.
+
+---
+### Usage
+
+Just put `ScriptTemplates` folder into `Assets/` and restart Unity Editor.
+
+![Demo](./screenshot1.png)
